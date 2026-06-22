ffa134335369974c1b60b5633d3e2d59e531b63d
anithchathusha22 <it20640798@my.sliit.lk>
2026-06-22 14:29:43 +0530

    update

--- src/Utils/AnimatedLine.jsx
+++ src/Utils/AnimatedLine.jsx
@@ -1,25 +1,75 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+const PATH_1_D =
+  "M-79.2008 370.518C-56.7435 366.255 -35.6572 367.317 -13.0986 359.175C34.7614 341.902 85.5207 330.973 133.973 314.956C210.788 289.565 294.796 277.255 376.629 267.981C430.726 261.85 486.404 259.496 542.126 260.701C590.735 261.752 635.305 249.258 682.901 246.346C784.707 240.117 876.493 197.056 977.445 186.501C1054.22 178.474 1128.85 162.885 1203.99 148.619C1228.15 144.032 1254.07 150.371 1279.07 147.641C1302.03 145.135 1323.81 136.734 1346.36 132.453C1385.37 125.046 1422.29 100.836 1462.89 100.672C1494.99 100.542 1525.11 87.6501 1556.29 81.7304C1610.3 71.4774 1662.76 56.0843 1716.87 45.8121C1754.98 38.5761 1791.53 29.2537 1828.62 19.4647C1844.16 15.3636 1857.96 8.16104 1873.83 5.1463C1877.18 4.51189 1908.27 -3.63428 1900.17 5.57843";
+
+const PATH_2_D =
+  "M11.8599 341.551C34.4933 338.102 53.2336 327.424 77.882 329.626C130.176 334.297 181.973 328.398 234.186 330.968C316.96 335.042 399.822 316.792 479.192 295.026C531.661 280.638 583.702 260.017 634.018 234.152C677.912 211.589 724.83 210.664 769.763 194.36C865.873 159.484 971.087 183.033 1068.56 154.88C1142.69 133.47 1218.61 124.091 1294.34 112.55C1318.69 108.839 1339.23 88.3208 1363.43 81.5173C1385.65 75.2714 1409.71 78.1915 1432.44 74.7282C1471.75 68.7364 1517.5 88.3234 1554.67 71.0119C1584.06 57.3221 1617.98 63.2266 1649.4 58.4376C1703.83 50.143 1759.4 50.0617 1813.93 41.7516C1852.34 35.8977 1890.36 33.7817 1929.1 32.1211C1945.33 31.4253 1961.51 36.0354 1977.51 33.5965C1980.88 33.0833 2013.33 31.6028 2001.36 21.5745";
+
+const LINE_1_DURATION = "3.9s";
+const LINE_2_DURATION = "3.9s";
+
+function LineSpark({ pathId, duration, begin = "0s" }) {
+  return (
+    <g className="mix-blend-screen">
+      <ellipse
+        rx="46"
+        ry="0.95"
+        fill="url(#spark-grad)"
+        filter="url(#spark-glow)"
+        opacity="0.55"
+      >
+        <animateMotion
+          dur={duration}
+          begin={begin}
+          repeatCount="indefinite"
+          rotate="auto"
+          calcMode="linear"
+          keyPoints="0;1"
+          keyTimes="0;1"
+        >
+          <mpath href={`#${pathId}`} />
+        </animateMotion>
+
+        <animate
+          attributeName="opacity"
+          values="0.45;0.9;0.45"
+          dur="1.6s"
+          repeatCount="indefinite"
+        />
+      </ellipse>
+
+      <ellipse
+        rx="18"
+        ry="0.5"
+        fill="#ffff66"
+        filter="url(#spark-glow)"
+        opacity="0.85"
+      >
+        <animateMotion
+          dur={duration}
+          begin={begin}
+          repeatCount="indefinite"
+          rotate="auto"
+          calcMode="linear"
+          keyPoints="0;1"
+          keyTimes="0;1"
+        >
+          <mpath href={`#${pathId}`} />
+        </animateMotion>
+
+        <animate
+          attributeName="opacity"
+          values="0.65;1;0.65"
+          dur="1.6s"
+          repeatCount="indefinite"
+        />
+      </ellipse>
+    </g>
+  );
+}
 
 export default function AnimatedLine() {
-  const path1Ref = useRef(null);
-  const path2Ref = useRef(null);
-
-  // Both sparks move at the same speed (px/sec)
-  const SPEED_PX_PER_SEC = 520;
-
-  const [dur1, setDur1] = useState("3.2s");
-  const [dur2, setDur2] = useState("3.2s");
-
-  useEffect(() => {
-    const l1 = path1Ref.current?.getTotalLength?.() ?? 0;
-    const l2 = path2Ref.current?.getTotalLength?.() ?? 0;
-
-    if (l1 > 0) setDur1(`${Math.max(1.2, l1 / SPEED_PX_PER_SEC).toFixed(2)}s`);
-    if (l2 > 0) setDur2(`${Math.max(1.2, l2 / SPEED_PX_PER_SEC).toFixed(2)}s`);
-  }, []);
-
   return (
     <div
       className="
@@ -97,9 +147,8 @@ export default function AnimatedLine() {
 
         {/* Base Paths */}
         <path
-          ref={path1Ref}
           id="path1"
-          d="M-79.2008 370.518C-56.7435 366.255 -35.6572 367.317 -13.0986 359.175C34.7614 341.902 85.5207 330.973 133.973 314.956C210.788 289.565 294.796 277.255 376.629 267.981C430.726 261.85 486.404 259.496 542.126 260.701C590.735 261.752 635.305 249.258 682.901 246.346C784.707 240.117 876.493 197.056 977.445 186.501C1054.22 178.474 1128.85 162.885 1203.99 148.619C1228.15 144.032 1254.07 150.371 1279.07 147.641C1302.03 145.135 1323.81 136.734 1346.36 132.453C1385.37 125.046 1422.29 100.836 1462.89 100.672C1494.99 100.542 1525.11 87.6501 1556.29 81.7304C1610.3 71.4774 1662.76 56.0843 1716.87 45.8121C1754.98 38.5761 1791.53 29.2537 1828.62 19.4647C1844.16 15.3636 1857.96 8.16104 1873.83 5.1463C1877.18 4.51189 1908.27 -3.63428 1900.17 5.57843"
+          d={PATH_1_D}
           stroke="url(#line1-gradient)"
           strokeWidth="1.9"
           strokeLinecap="round"
@@ -108,70 +157,16 @@ export default function AnimatedLine() {
         />
 
         <path
-          ref={path2Ref}
           id="path2"
-          d="M11.8599 341.551C34.4933 338.102 53.2336 327.424 77.882 329.626C130.176 334.297 181.973 328.398 234.186 330.968C316.96 335.042 399.822 316.792 479.192 295.026C531.661 280.638 583.702 260.017 634.018 234.152C677.912 211.589 724.83 210.664 769.763 194.36C865.873 159.484 971.087 183.033 1068.56 154.88C1142.69 133.47 1218.61 124.091 1294.34 112.55C1318.69 108.839 1339.23 88.3208 1363.43 81.5173C1385.65 75.2714 1409.71 78.1915 1432.44 74.7282C1471.75 68.7364 1517.5 88.3234 1554.67 71.0119C1584.06 57.3221 1617.98 63.2266 1649.4 58.4376C1703.83 50.143 1759.4 50.0617 1813.93 41.7516C1852.34 35.8977 1890.36 33.7817 1929.1 32.1211C1945.33 31.4253 1961.51 36.0354 1977.51 33.5965C1980.88 33.0833 2013.33 31.6028 2001.36 21.5745"
+          d={PATH_2_D}
           stroke="#FBFD00"
           strokeWidth="1.5"
           strokeLinecap="round"
           vectorEffect="non-scaling-stroke"
         />
 
-        {/* Spark 1 (Line 1) - Start together, Left -> Right */}
-        <ellipse
-          rx="30"
-          ry="0.55"
-          fill="url(#spark-grad)"
-          filter="url(#spark-glow)"
-          className="mix-blend-screen"
-        >
-          <animateMotion
-            dur={dur1}
-            begin="0s"
-            repeatCount="indefinite"
-            rotate="auto"
-            calcMode="linear"
-            keyPoints="0;1"
-            keyTimes="0;1"
-          >
-            <mpath href="#path1" />
-          </animateMotion>
-
-          <animate
-            attributeName="opacity"
-            values="0.55;0.95;0.55"
-            dur="1.8s"
-            repeatCount="indefinite"
-          />
-        </ellipse>
-
-        {/* Spark 2 (Line 2) - Start together, Left -> Right */}
-        <ellipse
-          rx="30"
-          ry="0.55"
-          fill="url(#spark-grad)"
-          filter="url(#spark-glow)"
-          className="mix-blend-screen"
-        >
-          <animateMotion
-            dur={dur2}
-            begin="0s"
-            repeatCount="indefinite"
-            rotate="auto"
-            calcMode="linear"
-            keyPoints="0;1"
-            keyTimes="0;1"
-          >
-            <mpath href="#path2" />
-          </animateMotion>
-
-          <animate
-            attributeName="opacity"
-            values="0.55;0.95;0.55"
-            dur="1.8s"
-            repeatCount="indefinite"
-          />
-        </ellipse>
+        <LineSpark pathId="path1" duration={LINE_1_DURATION} />
+        <LineSpark pathId="path2" duration={LINE_2_DURATION} />
       </svg>
     </div>
   );
